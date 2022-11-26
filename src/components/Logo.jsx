3eed28cff6dfaca3afa3cd48b8ad777f26e77ac2
SvoronaM <sergeymigal@gmail.com>
2022-11-26 22:35:39 +0100

    logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,15 +5,15 @@ import { useNavigate } from 'react-router-dom';
 
 
 const Logo = () => {
-    const navigate = useNavigate()
-    const [size, setSize] = useState(window.innerWidth)
+    const navigate = useNavigate();
+    const [size, setSize] = useState(window.innerWidth);
     useEffect(() => {
         const handleResize = () => {
-            setSize(window.innerWidth)
+            setSize(window.innerWidth);
         }
         window.addEventListener("resize", handleResize);
         return () => {
-            window.removeEventListener("resize", handleResize)
+            window.removeEventListener("resize", handleResize);
         }
     },[])
     return (

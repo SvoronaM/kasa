0c714dd0579033fd0552cc16922686f7cb0ffb2a
SvoronaM <sergeymigal@gmail.com>
2022-11-26 21:25:21 +0100

    logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,24 +1,23 @@
 import { useEffect, useState } from 'react';
 import LogoMobile from '../images/logoMobile.png';
-import LogoImg from '../images/logo.png';
-import { useNavigate }  from 'react-router-dom';
+import LogoImg from '../images/logo.png'
+import { useNavigate } from 'react-router-dom';
 
 
 const Logo = () => {
-    const navigate = useNavigate();
-    const [size, setSize] = useState(window.innerWidth);
+    const navigate = useNavigate()
+    const [size, setSize] = useState(window.innerWidth)
     useEffect(() => {
         const handleResize = () => {
-            setSize(window.innerWidth);
+            setSize(window.innerWidth)
         }
         window.addEventListener("resize", handleResize);
         return () => {
-            window.removeEventListener("resize", handleResize);
+            window.removeEventListener("resize", handleResize)
         }
     },[])
     return (
-        size < 375 ? <img src={LogoMobile} alt="logo de Kasa" onClick={() => navigate('/')}/ > : <img src={LogoImg} alt="logo de Kasa" onClick={() => navigate('/')}/ >
-    );
-}
-
+        size < 375 ? <img src={LogoMobile} alt="logo de Kasa" onClick={() => navigate('/')}/ > : <img src={LogoImg} alt="logo de Kasa" onClick={() => navigate('/')}/>
+            );
+ }
 export default Logo;

e4f9f9e651b4183f3dfb7e52bf06e1ff72809444
SvoronaM <sergeymigal@gmail.com>
2022-11-29 18:22:10 +0100

    Caroussel.jsx

--- src/components/Caroussel.jsx
+++ src/components/Caroussel.jsx
@@ -42,7 +42,7 @@ const Caroussel = ({ images }) => {
 
     return (
         <div className='caroussel'>
-            {dataLength> 1 && <div className='caroussel-left' onClick={()=> prevPic()}><i className="fa-solid fa-chevron-left"></i></div>}
+            {dataLength > 1 && <div className='caroussel-left' onClick={()=> prevPic()}><i className="fa-solid fa-chevron-left"></i></div>}
             {dataLength > 1 && <div className='caroussel-right' onClick={() => nextPic()}><i className="fa-solid fa-chevron-right"></i></div>}
             <img src={`${picData[picNumber-1]}`} alt=""/>
             {dataLength > 1 && <div className='caroussel-count'>
